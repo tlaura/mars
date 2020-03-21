3eba7a4152477b926479c311cefb47288b68a376
Pecske <pecske92@gmail.com>
2020-03-21 16:49:49 +0100

    refactor

--- mars-frontend/src/environments/environment.prod.ts
+++ mars-frontend/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   apiKey: 'AIzaSyAeQEHjfxVD8584aJATT0pSNBf3DuCr1h0',
 //  BASE_URL: "https://54.229.84.224"
-  BASE_URL: "https://mars-demo.progmasters.hu/"
+  BASE_URL: "https://mars-demo.progmasters.hu"
 };
