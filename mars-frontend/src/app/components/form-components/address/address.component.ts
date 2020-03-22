913978251d50bd9b3f754a817307502cd02051b9
Pecske <pecske92@gmail.com>
2020-03-22 08:30:02 +0100

    refactor

--- mars-frontend/src/app/components/form-components/address/address.component.ts
+++ mars-frontend/src/app/components/form-components/address/address.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, HostListener, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {FormControl, Validators} from "@angular/forms";
 
 @Component({
@@ -31,28 +31,19 @@ export class AddressComponent implements OnInit {
     this.addressEmitter = new EventEmitter<string>();
   }
 
-  @HostListener('animationstart')
-  onAutoFillStart() {
-    this.emitZipcode();
-    this.emitCity();
-    this.emitAddress();
-  }
 
   ngOnInit(): void {
   }
 
   emitZipcode() {
-    console.log("zipcode emitted");
     this.zipcodeEmitter.emit(this.zipcode.value)
   }
 
   emitCity() {
-    console.log("city emitted");
     this.cityEmitter.emit(this.city.value);
   }
 
   emitAddress() {
-    console.log("address emitted");
     this.addressEmitter.emit(this.address.value);
   }
 
