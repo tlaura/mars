c3befddfe67319ba062b0572e07ba4014500523b
Hudák Elza <hudak.elza@gmail.com>
2020-03-26 11:06:09 +0100

    fix list

--- mars-frontend/src/app/components/institution-components/institution-list/institution-list.component.ts
+++ mars-frontend/src/app/components/institution-components/institution-list/institution-list.component.ts
@@ -154,7 +154,6 @@ export class InstitutionListComponent implements OnInit {
   }
 
   hasProviders(institution: AccountInstitutionListModel): boolean {
-    //TODO: length->provider list length...
-    return institution.providerServiceName?.length > 0 && institution.accountType != 'PROVIDER';
+    return institution.providers?.length > 0 && institution.accountType != 'PROVIDER';
   }
 }
