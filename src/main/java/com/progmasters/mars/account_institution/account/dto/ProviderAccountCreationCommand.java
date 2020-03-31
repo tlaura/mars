268f686428b26dc0fffda6a75b39c168d2df3984
Hudák Elza <hudak.elza@gmail.com>
2020-03-31 13:46:23 +0200

    merge

--- src/main/java/com/progmasters/mars/account_institution/account/dto/ProviderAccountCreationCommand.java
+++ src/main/java/com/progmasters/mars/account_institution/account/dto/ProviderAccountCreationCommand.java
@@ -2,6 +2,7 @@ package com.progmasters.mars.account_institution.account.dto;
 
 import com.progmasters.mars.account_institution.institution.dto.InstitutionCreationCommand;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -14,6 +15,7 @@ import java.util.List;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class ProviderAccountCreationCommand {
 
     @NotBlank
