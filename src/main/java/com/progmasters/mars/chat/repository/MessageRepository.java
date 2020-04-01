d9cd21ef81d4dc27bbecaed266565f5d05c8d34f
Pecske <pecske92@gmail.com>
2020-04-01 15:49:35 +0200

    add repository

---
+++ src/main/java/com/progmasters/mars/chat/repository/MessageRepository.java
@@ -0,0 +1,9 @@
+package com.progmasters.mars.chat.repository;
+
+import com.progmasters.mars.chat.domain.Message;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MessageRepository extends JpaRepository<Message, Long> {
+}
