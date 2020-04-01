bad24d1f71a4ccb86df1759895e5837e4741548d
Pecske <pecske92@gmail.com>
2020-04-01 18:09:30 +0200

    add controller

--- src/main/java/com/progmasters/mars/chat/controller/ChatController.java
+++ src/main/java/com/progmasters/mars/chat/controller/ChatController.java
@@ -1,18 +1,30 @@
 package com.progmasters.mars.chat.controller;
 
 import com.progmasters.mars.chat.dto.MessageData;
+import com.progmasters.mars.chat.service.ChatService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
 @Controller
+@Slf4j
 public class ChatController {
+
+    private final ChatService chatService;
+
+    @Autowired
+    public ChatController(ChatService chatService) {
+        this.chatService = chatService;
+    }
 
     @MessageMapping("/sendMessage")
     @SendTo("/topic")
     public MessageData sendMessage(@Payload MessageData messageData) {
 
+        chatService.saveMessage(messageData);
 
         return messageData;
     }
--- src/main/java/com/progmasters/mars/chat/repository/ContactRepository.java
+++ src/main/java/com/progmasters/mars/chat/repository/ContactRepository.java
@@ -2,9 +2,13 @@ package com.progmasters.mars.chat.repository;
 
 import com.progmasters.mars.chat.domain.Contact;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ContactRepository extends JpaRepository<Contact, Long> {
 
+    @Query("select c from Contact c where (c.fromUser.email= :fromEmail and c.toUser.email= :toEmail) or (c.toUser.email= :fromEmail and c.fromUser.email= :toEmail)")
+    ContactRepository findConnectionByUsers(@Param("fromEmail") String fromEmail, @Param("toEmail") String toEmail);
 }
