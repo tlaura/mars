95f85b0439b36cca0429fce9b222bd28d6ce1d46
laura <tundelaura93@gmail.com>
2020-04-01 13:54:35 +0100

    Add name to JWT token

--- src/main/java/com/progmasters/mars/account_institution/account/security/JwtTokenProvider.java
+++ src/main/java/com/progmasters/mars/account_institution/account/security/JwtTokenProvider.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Component;
 
 import java.io.Serializable;
 import java.util.Date;
-
 import java.util.HashMap;
 import java.util.Map;
 
@@ -29,6 +28,7 @@ public class JwtTokenProvider implements Serializable {
         Date expiryDate = new Date(now.getTime() + JWT_TOKEN_VALIDITY);
 
         Map<String, Object> claims = new HashMap<>();
+        claims.put("name", userPrincipal.getUsername());
         claims.put("role", userPrincipal.getRole());
 
         return Jwts.builder()
