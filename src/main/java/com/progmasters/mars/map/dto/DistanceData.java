28b4769914119be3f6ba00bed5a47dc802f86716
Pecske <pecske92@gmail.com>
2020-04-04 21:08:13 +0200

    optimize list load in with completablefuture

--- src/main/java/com/progmasters/mars/map/dto/DistanceData.java
+++ src/main/java/com/progmasters/mars/map/dto/DistanceData.java
@@ -1,6 +1,8 @@
 package com.progmasters.mars.map.dto;
 
 import com.google.maps.model.Duration;
+import com.google.maps.model.TravelMode;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -9,18 +11,12 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class DistanceData {
 
-    private Duration travelTimeByDriving;
+    private TravelMode travelMode;
 
-    private Long distanceByDriving;
+    private Long distance;
 
-    private Duration travelTimeByWalking;
-
-    private Long distanceByWalking;
-
-    private Duration travelTimeByTransit;
-
-    private Long distanceByTransit;
-
+    private Duration travelTime;
 }
